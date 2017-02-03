ddaa59235916a94228dd5e78928f52116798f275
slash6475 <duhart@mit.edu>
2017-02-03 12:30:33 -0500

    Refactor VGG Analyzer

--- src/analyzerVGG.py
+++ src/analyzerVGG.py
@@ -13,62 +13,72 @@ config = tf.ConfigProto(
     )
 dropout = 1
 
-#Create input places
-with tf.name_scope('input'):
-    X = tf.placeholder(tf.float32, [None, 150*186])
-    y = tf.placeholder(tf.float32, [None, 50])
-    keep_prob = tf.placeholder(tf.float32)
 
-### Build the Neural Net model
-with tf.name_scope('VGG'):
-    tf.summary.scalar('dropout_keep_probability', keep_prob)
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-    pred, biases, weights = net.vgg(X, dropout, 50,
-            data_size=(150,186))
-    correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-    tf.summary.scalar('accuracy', accuracy)
 
-init = tf.global_variables_initializer()
-
-# Create an runnable object for the streamer
 class AnalyzerVGG:
     def __init__(self, checkpoint_dir, label_dictionary):
         self.label_dic = label_dictionary
-        self.session = session = tf.InteractiveSession(config=config)
-        self.session.run(init)
+        self.dataw = 150
+        self.datah = 186
 
-        # Load the session of the neural network
+        # Load the network
+        self.load()
+
+        # Load the session
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir + "/")
         if ckpt and ckpt.model_checkpoint_path:
             saver = tf.train.Saver(max_to_keep=2)
+            print(ckpt.model_checkpoint_path)
             saver.restore(self.session, ckpt.model_checkpoint_path)
             print("Previous session loaded")
         else:
             print("Not Neural Network found in " + checkpoint_dir)
             quit()
 
+    # Load the network
+    def load(self):
+        #Create input places
+        with tf.name_scope('input'):
+            self.X = tf.placeholder(tf.float32, [None, self.dataw*self.datah])
+            self.y = tf.placeholder(tf.float32, [None, self.label_dic.shape[0]])
+            keep_prob = tf.placeholder(tf.float32)
+
+        ### Build the Neural Net model
+        with tf.name_scope('VGG'):
+            tf.summary.scalar('dropout_keep_probability', keep_prob)
+            global_step = tf.Variable(0, trainable=False, name='global_step')
+            self.pred, biases, weights = net.vgg(self.X, dropout, self.label_dic.shape[0],
+                    data_size=(self.dataw,self.datah))
+            correct_pred = tf.equal(tf.argmax(self.pred, 1), tf.argmax(self.y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+            tf.summary.scalar('accuracy', accuracy)
+
+        init = tf.global_variables_initializer()
+        self.session = session = tf.InteractiveSession(config=config)
+        self.session.run(init)
+
     # Function called by the streamer to predic its current sample
     def run(self, Sxx, fs, t, sound_obj):
         Sxx = np.reshape(Sxx, [1, Sxx.shape[0]*Sxx.shape[1]] )
-        res = self.session.run(pred,feed_dict={X: Sxx} )
+        res = self.session.run(self.pred,feed_dict={self.X: Sxx} )
         a = np.argmax(res)
         print(self.label_dic[a])
 
+
 if __name__ == "__main__":
     parser = optparse.OptionParser()
-    parser.set_defaults(play=False,dic=False,nn=False)
+    parser.set_defaults(play=False,dic=False,nn="checkpoints/")
     parser.add_option("-p", "--play", action="store", type="string", dest="play")
     parser.add_option("-d", "--dic", action="store", type="string", dest="dic")
     parser.add_option("-n", "--nn", action="store", type="string", dest="nn")
     (options, args) = parser.parse_args()
 
-    checkpoint_dir = "checkpoints"
+    checkpoint_dir = options.nn
 
     if options.play and options.dic and options.nn:
         label_dic = np.load(options.dic + "_labels_dic.npy")
         player = AnalyzerVGG(options.nn, label_dic)
         tiddata.play_spectrogram_from_stream(options.play,
-                show=False, callable_objects = [player])
+                show=True, callable_objects = [player])
     else:
         print('Wrong options : ')
--- src/data.py
+++ src/data.py
@@ -173,8 +173,11 @@ class Dataset:
         b = ((self.cur_batch_train+1)*batch_size) % self.data_train.shape[0]
         batch_x = self.data_train[ [x for x in range(a,b)], :]
         batch_y = self.label_train[ [x for x in range(a,b)], :]
-        print("Batch #{0}: {1} samples of {2} features".format(
-                self.cur_batch_train, batch_x.shape[0],batch_x.shape[1]))
+        print("#({3}) Batch #{0}: {1} samples of {2} features".format(
+                self.cur_batch_train,
+                batch_x.shape[0],
+                batch_x.shape[1],
+                self.cur_batch_train/self.data_train.shape[0] ))
         self.cur_batch_train += 1
         return batch_x, batch_y
 
