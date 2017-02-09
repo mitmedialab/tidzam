33367d11bd3cc7dd7e9b44752e72b91ee1d92a72
slash6475 <duhart@mit.edu>
2017-02-09 01:47:52 -0500

    Multi-model of Neural Network Support

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,8 @@
-__all__ = ["vgg", "test"]
+import glob
+import os
+
+__all__ = []
+for f in glob.glob(os.path.dirname(os.path.realpath(__file__)) + "/*.py"):
+    model = os.path.basename(f)
+    if model != '__init__.py':
+        __all__.append(model.split('.')[0])
--- src/models/selector.py
+++ src/models/selector.py
@@ -3,9 +3,7 @@ import tflearn
 
 class DNN:
     def __init__(self,data_size, n_classes):
-        self.show_kernel_map = []
-
-        self.name = "DNN"
+        self.name = "selector"
         self.img_prep = tflearn.ImagePreprocessing()
         # Zero Center (With mean computed over the whole dataset)
         self.img_prep.add_featurewise_zero_center()
@@ -24,17 +22,11 @@ class DNN:
 
         net = tf.reshape(self.input, shape=[-1, data_size[0], data_size[1], 1])
         # Conv 1 - 2
-        self.conv1 = tflearn.conv_2d(net, 23, 7, activation='relu', regularizer='L2', name="conv1")
-        self.show_kernel_map.append(self.conv1)
+        self.conv1 = tflearn.conv_2d(net, 32, 5, activation='relu', regularizer='L2', name="conv1")
+        # Pooling 1
         self.pool1 = tflearn.layers.conv.max_pool_2d (self.conv1, 2, strides=2, padding='same', name='pool1')
-
-        self.conv2 = tflearn.conv_2d(self.pool1, 15, 5, activation='relu', regularizer='L2', name="conv2")
-        self.show_kernel_map.append(self.conv2)
-        self.pool2 = tflearn.layers.conv.max_pool_2d (self.conv2, 2, strides=2, padding='same', name='pool2')
-
-
         # Conv 3 - 4
         # Fully Connected
-        self.fc1 = tflearn.fully_connected(self.pool2, 30, activation='relu', name="fc1")
+        self.fc1 = tflearn.fully_connected(self.pool1, 128, activation='relu', name="fc1")
         self.drop1 = tflearn.layers.core.dropout (self.fc1, 0.5, name='Dropout1')
         self.out = tflearn.fully_connected(self.drop1, n_classes, activation='linear', name="out")
