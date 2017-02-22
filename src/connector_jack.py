cbe276903959d4e9c5bba10afdec0b7c21967ea0
slash6475 <duhart@mit.edu>
2017-02-22 14:31:51 -0500

    FIX Bug of Real Time requirement for Jack Python librairy with a Ring Buffer

--- src/connector_jack.py
+++ src/connector_jack.py
@@ -1,19 +1,17 @@
 import jack
 import sys
 import numpy as np
-import Queue
-import multiprocessing as mp
 import threading
-
-from matplotlib import pyplot as plt
+from threading import Thread
 
 import data as tiddata
 
-class TidzamJack:
+class TidzamJack(Thread):
     def __init__(self, port_name, callable_objects=[]):
+        Thread.__init__(self)
+
         self.data_size = (150,186)
         self.callable_objects = callable_objects
-        self.lock = threading.Lock()
 
         self.client = jack.Client("tidzam")
         self.ports = self.client.get_ports(port_name)
@@ -21,23 +19,32 @@ class TidzamJack:
         self.samplerate = -1
         self.blocksize = -1
         self.buffer_size = 24000
+        self.buffer_jack = self.buffer_size * 20
 
         self.channels = []
+        self.ring_buffer = []
         self.channels_data = []
+
+        self.stopFlag = threading.Event()
 
         for i in range(0, len(self.ports)):
             self.channels.append(self.client.inports.register("chan"+str(i)))
+            self.ring_buffer.append(jack.RingBuffer(self.buffer_jack))
 
         self.client.set_samplerate_callback(self.callback_samplerate)
         self.client.set_blocksize_callback(self.callback_blocksize)
-        self.client.set_process_callback(self.callback)
 
+        self.client.set_process_callback(self.callback_rt)
 
-    def start(self):
         self.client.activate()
 
         for i in range(0, len(self.ports)): #p in ports:
             self.client.connect(self.ports[i], self.channels[i])
+
+    def stop(self):
+        self.client.deactivate()
+        self.client.close()
+        self.stopFlag.set()
 
     def callback_samplerate(self, samplerate):
         self.samplerate = samplerate
@@ -47,39 +54,43 @@ class TidzamJack:
         self.blocksize = blocksize
         print("Blocksize: " + str(blocksize))
 
-    def callback(self, frame):
+    def callback_rt(self,frame):
+        for i in range(len(self.channels)):
+                self.ring_buffer[i].write(self.channels[i].get_array())
+
+    def run(self):
         run = False
+        while not self.stopFlag.wait(0.1):
+            for i in range(len(self.channels)):
+                data = self.ring_buffer[i].read(self.buffer_jack)
+                data = np.frombuffer(data, dtype='float32')
+                try:
+                    self.channels_data[i] = np.concatenate((self.channels_data[i], data ))
+                except:
+                    self.channels_data.append( data )
 
-        self.lock.acquire()
-        for i in range(0, len(self.channels)):
-            # Store the received data in the corresponding buffer or create one
-            try:
-                self.channels_data[i] = np.concatenate((self.channels_data[i], self.channels[i].get_array()))
-            except:
-                self.channels_data.append( self.channels[i].get_array().tolist() )
+                # If the buffer contains the required data, we truncate it and send result
+                if len(self.channels_data[i]) >= self.buffer_size:
+                    run = True
+                    data = self.channels_data[i][0:self.buffer_size]
+                    self.channels_data[i] = self.channels_data[i][self.buffer_size:len(self.channels_data[i])]
+                    fs, t, Sxx = tiddata.get_spectrogram(data, self.samplerate)
 
-            # If the buffer contains the required data, we truncate it and send result
-            if len(self.channels_data[i]) >= self.buffer_size:
-                run = True
-                data = np.transpose(self.channels_data[i][0:self.buffer_size])
-                self.channels_data[i] = self.channels_data[i][self.buffer_size:len(self.channels_data[i])]
-
-                #print("Buffer " + str(i) + ": "+ str(len(data)) + " Bytes" )
-                fs, t, Sxx = tiddata.get_spectrogram(data.astype(float),
-                            self.samplerate)
-
-                if i == 0:
-                    datas = np.transpose(data)
-                    Sxxs = Sxx
-                    fss = fs
-                    ts = t
+                    if i == 0:
+                        print("-----------------------------------")
+                        print("Buffer load: " + str(int(len(self.channels_data[i])/self.buffer_size)) + " samples" )
+                        datas   = data
+                        Sxxs    = Sxx
+                        fss     = fs
+                        ts      = t
+                    else:
+                        Sxxs    = np.vstack((Sxxs, Sxx))
+                        fss     = np.vstack((fss, fs))
+                        ts      = np.vstack((ts, t))
+                        datas   = np.vstack((datas, data))
                 else:
-                    Sxxs = np.vstack((Sxxs, Sxx))
-                    fss = np.vstack((fss, fs))
-                    ts = np.vstack((ts, t))
-                    datas = np.vstack((datas, data))
+                    run = False
 
-        if run is True:
-            for obj in self.callable_objects:
-                obj.execute(Sxxs, fss, ts, [np.transpose(datas), self.samplerate], overlap=0)
-        self.lock.release()
+            if run is True:
+                for obj in self.callable_objects:
+                    obj.execute(Sxxs, fss, ts, [np.transpose(datas), self.samplerate], overlap=0)
