5dca30029e6bd84a02f430a5e0c6ba8196c78fa1
slash6475 <duhart@mit.edu>
2017-05-09 14:06:00 -0400

    Play all files contained in a given folder

--- src/input_audiofile.py
+++ src/input_audiofile.py
@@ -1,14 +1,19 @@
 import soundfile as sf
 import numpy as np
 from threading import Thread
-import os
+import os, glob
 
 import data as tiddata
 
 class TidzamAudiofile(Thread):
-    def __init__(self, audiofilename, callable_objects=[], overlap=0, channel=None):
+    def __init__(self, streams, callable_objects=[], overlap=0, channel=None):
         Thread.__init__(self)
-        self.audiofilename      = audiofilename
+
+        if os.path.isdir(streams) is True:
+            self.filenames = glob.glob(streams+"/*")
+        else:
+            self.filenames = [streams]
+
         self.callable_objects   = callable_objects
         self.overlap            = overlap
         self.channel            = channel
@@ -17,37 +22,39 @@ class TidzamAudiofile(Thread):
         return
 
     def run(self):
-        with sf.SoundFile(self.audiofilename, 'r') as f:
-            sample_size = 24000 # 500ms
+        for audiofilename in self.filenames:
+            with sf.SoundFile(audiofilename, 'r') as f:
+                sample_size = 24000 # 500ms
 
-            if self.channel is None:
-                channels = range(0,f.channels)
-            else:
-                channels = range(self.channel,self.channel+1)
+                if self.channel is None:
+                    channels = range(0,f.channels)
+                else:
+                    channels = range(self.channel,self.channel+1)
 
-            while f.tell() < len(f):
-                data = f.read(sample_size)
+                while f.tell() < len(f):
+                    data = f.read(sample_size)
 
-                if (len(data) < sample_size):
-                    print("End of stream ...")
-                    os._exit(0)
+                    if (len(data) < sample_size):
+                        break
 
-                for i in channels:
-                    if f.channels > 1:
-                        fs, t, Sxx = tiddata.get_spectrogram(data[:,i], f.samplerate, i)
-                    else:
-                        fs, t, Sxx = tiddata.get_spectrogram(data, f.samplerate, i)
+                    for i in channels:
+                        if f.channels > 1:
+                            fs, t, Sxx = tiddata.get_spectrogram(data[:,i], f.samplerate, i)
+                        else:
+                            fs, t, Sxx = tiddata.get_spectrogram(data, f.samplerate, i)
 
-                    if i == 0:
-                        Sxxs = Sxx
-                        fss = fs
-                        ts = t
-                    else:
-                        Sxxs = np.concatenate((Sxxs, Sxx), axis=0)
-                        fss = np.concatenate((fss, fs), axis=0)
-                        ts = np.concatenate((ts, t), axis=0)
+                        if i == 0:
+                            Sxxs = Sxx
+                            fss = fs
+                            ts = t
+                        else:
+                            Sxxs = np.concatenate((Sxxs, Sxx), axis=0)
+                            fss = np.concatenate((fss, fs), axis=0)
+                            ts = np.concatenate((ts, t), axis=0)
 
-                for obj in self.callable_objects:
-                    obj.execute(Sxxs, fss, ts, [data, f.samplerate], overlap=self.overlap,stream=self.audiofilename)
+                    for obj in self.callable_objects:
+                        obj.execute(Sxxs, fss, ts, [data, f.samplerate], overlap=self.overlap,stream=audiofilename)
 
-                f.seek(int(-sample_size*self.overlap), whence=sf.SEEK_CUR)
+                    f.seek(int(-sample_size*self.overlap), whence=sf.SEEK_CUR)
+        print("End of stream ...")
+        os._exit(0)
